4d2e61cca72fc85d435cd0040b76c9a5498d444b
mooreayden3214-glitch <mooreayden3214@gmail.com>
2026-09-15 21:28:41 -0400

    server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get("/return", async (req, reply) =>
 app.setNotFoundHandler((req, reply) =>
   req.raw.method === "GET" &&
   req.headers.accept?.includes("text/html")
-    ? reply.sendFile("public/pages/index.html")
+    ? reply.sendFile("pages/index.html")
     : reply.code(404).send({ error: "Not Found" })
 );
 
